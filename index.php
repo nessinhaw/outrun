c55702a7a5a6b891a88f4ed57e428a8380ffbb95
Nessa <nessastagg@gmail.com>
2019-03-28 14:24:17 -0300

    staaaaarting yet another new oneeee

---
+++ index.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Hello Bulma!</title>
+    <link rel="stylesheet" href="css/bulma-0.7.4/css/bulma.min.css">
+    <link rel="stylesheet" href="css/theme.css">
+    <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+  </head>
+  <body>
+  <!--Navbar-->  
+  <nav class="level is-nav">
+    <p class="level-item is-nav-title has-text-centered">
+        Nameless
+    </p>
+  </nav>
+  <nav class="is-menu has-text-centered">
+    <p>
+      <a class="is-menu-item" href="">Home</a>
+
+      <a class="is-menu-item" href="">Notícias</a>
+  
+      <a class="is-menu-item" href="">Reviews</a>
+  
+      <a class="is-menu-item" href="">Guias</a>
+ 
+      <a class="is-menu-item" href="">Sobre</a>
+    </p>
+  </nav>
+  <!--Featured-->
+  <section class="hero">
+    <div class="hero-body">
+      <div class="container">
+        <div class="card is-fullimage">
+          <div class="card-opacity"></div>
+            <div class="card-image">
+              <figure class="image is-2by1">
+                <img src="https://img.finalfantasyxiv.com/lds/promo/h/l/Wpxdd7E1zDgSN7d4ITTbSMymfI.png" alt="Placeholder image">
+              </figure>
+            </div>
+            <div class="card-stacked">
+              <div class="card-content">
+                <p class="media-spacing">
+                  <div class="media-tags">
+                     <a class="tag">Noticias</a>
+                  </div>
+                  <h1><a href="">Titulo do Post</a></h1>
+                  <small>Em 26/01/2018 por <a href="">Yuck</a></small>
+                </p>
+                <div class="content">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim nunc faucibus a pellentesque sit amet. Nunc eget lorem dolor sed. Non pulvinar neque laoreet suspendisse interdum. Non sodales neque sodales ut etiam sit amet. Potenti nullam ac tortor vitae purus faucibus ornare suspendisse. <a href="">@bulmaio</a>.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+      </div>
+    </div>
+  </section>
+  <!--Posts-->
+  <section class="section">
+    <div class="container">
+      <div class="is-divider">Recentes</div>
+      <div class="columns">
+        <div class="column is-three-quarters">
+          <article class="media media-card-posts">
+            <figure class="media-left media-image">
+              <p class="image is-128x128">
+                <img src="https://bulma.io/images/placeholders/128x128.png">
+              </p>
+            </figure>
+            <div class="media-content">
+              <div class="content">
+                <p class="media-spacing">
+                  <span class="tags is-pulled-right media-tags">
+                    <a class="tag">Reviews</a>
+                  </span>
+                  <strong class="media-title">QTitulo do Post</strong> <br>
+                  <small>Em 26/01/2018</small> <small>por <a href="">Yuck</a></small>
+                </p>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim nunc faucibus a pellentesque sit amet. Nunc eget lorem dolor sed. Non pulvinar neque laoreet suspendisse interdum. Non sodales neque sodales ut etiam sit amet. Potenti nullam ac tortor vitae purus faucibus ornare.
+                  <a href="">Leia mais</a>
+                </p>
+              </div>
+            </div>
+            <div class="media-right">
+            </div>
+          </article>
+          <article class="media media-card-posts">
+            <figure class="media-left media-image">
+              <p class="image is-128x128">
+                <img src="https://bulma.io/images/placeholders/128x128.png">
+              </p>
+            </figure>
+            <div class="media-content">
+              <div class="content">
+                <p class="media-spacing">
+                  <span class="tags is-pulled-right media-tags">
+                    <a class="tag">Noticias</a>
+                  </span>
+                  <strong class="media-title">QTitulo do Post</strong> <br>
+                  <small>Em 26/01/2018</small> <small>por <a href="">Yuck</a></small>
+                </p>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim nunc faucibus a pellentesque sit amet. Nunc eget lorem dolor sed. Non pulvinar neque laoreet suspendisse interdum. Non sodales neque sodales ut etiam sit amet. Potenti nullam ac tortor vitae purus faucibus ornare.
+                  <a href="">Leia mais</a>
+                </p>
+              </div>
+            </div>
+            <div class="media-right">
+            </div>
+          </article>
+          <article class="media media-card-posts">
+            <figure class="media-left media-image">
+              <p class="image is-128x128">
+                <img src="https://bulma.io/images/placeholders/128x128.png">
+              </p>
+            </figure>
+            <div class="media-content">
+              <div class="content">
+                <p class="media-spacing">
+                  <span class="tags is-pulled-right media-tags">
+                    <a class="tag">Guias</a>
+                  </span>
+                  <strong class="media-title">QTitulo do Post</strong> <br>
+                  <small>Em 26/01/2018</small> <small>por <a href="">Yuck</a></small>
+                </p>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim nunc faucibus a pellentesque sit amet. Nunc eget lorem dolor sed. Non pulvinar neque laoreet suspendisse interdum. Non sodales neque sodales ut etiam sit amet. Potenti nullam ac tortor vitae purus faucibus ornare.
+                  <a href="">Leia mais</a>
+                </p>
+              </div>
+            </div>
+            <div class="media-right">
+            </div>
+          </article>
+        </div>
+        <!--Sidebar-->
+        <div class="column">
+          <p>Something.</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  </body>
+</html>
